ca0e6c0c91997d0b8b9af53cd59d25af99cebaf9
Tran Ngoc Van <van.tran@easesolutions.com>
2011-10-17 00:07:59 +0800

    Creates new resource file when it doesn't exists.

--- Po2Resx/ResourcesManager.cs
+++ Po2Resx/ResourcesManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Xml.Linq;
 
 namespace Po2Resx
@@ -21,93 +22,42 @@ namespace Po2Resx
         {
             var resources = strings
                 .GroupBy(x => new StringGroup(x))
-                .Select(x => new
+                .Select(x => new ResourceInfo
                 {
                     Group = x.Key,
+                    Path = GetFilePath(x.Key),
                     Strings = x.ToDictionary(
-                        y => y.Key, y => y.Value),
+                        y => y.Key, y => y.Value)
                 })
                 .ToList();
 
             foreach (var resource in resources)
             {
-                var hasChanges = false;
-                var group = resource.Group;
-                var path = GetFilePath(group);
-
-                var doc = XDocument.Load(path);
-                var nsXml = XNamespace.Get("xml");
-                var ns = XNamespace.Get(string.Empty);
-
-                var root = doc.Root;
-                if (root == null)
+                if (File.Exists(resource.Path))
                 {
-                    throw new IOException(
-                        "Invalid resource file: " + path);
+                    Update(resource);
+                    continue;
                 }
 
-                var existing = root
-                    .Elements(ns + "data")
-                    .Where(x => x.Attribute("type") == null)
-                    .Select(x => new
-                    {
-                        Element = x,
-                        Name = x.Attribute("name"),
-                    })
-                    .Where(x => x.Name != null)
-                    .Select(x => new
-                    {
-                        x.Element,
-                        Name = x.Name.Value,
-                    })
-                    .Where(x => !x.Name.StartsWith(">>"))
-                    .ToDictionary(x => x.Name, x => x.Element);
+                Create(resource);
+            }
+        }
 
-                if (!IsDefault(group))
-                {
-                    /* Not default language
-                     * Remove untranslated keys */
+        private static void Create(ResourceInfo resource)
+        {
+            var path = resource.Path;
 
-                    var removes = existing.Keys
-                        .Except(resource.Strings.Keys)
-                        .ToList();
-
-                    foreach (var remove in removes)
-                    {
-                        hasChanges = true;
-                        existing[remove].Remove();
-                        existing.Remove(remove);
-                    }
-                }
-
+            using (var fs = File.Create(path))
+            using (var writer = new ResXResourceWriter(fs))
+            {
                 foreach (var item in resource.Strings)
                 {
-                    XElement element;
-                    if (existing.TryGetValue(item.Key, out element))
-                    {
-                        var valueElement = element.Element(ns + "value");
-                        if (valueElement != null && valueElement.Value != item.Value)
-                        {
-                            hasChanges = true;
-                            valueElement.Value = item.Value;
-                        }
-                    }
-                    else
-                    {
-                        hasChanges = true;
-                        root.Add(new XElement(ns + "data",
-                            new XAttribute("name", item.Key),
-                            new XAttribute(nsXml + "space", "preserve"),
-                            new XElement(ns + "value", item.Value)));
-                    }
+                    writer.AddResource(
+                        item.Key, item.Value);
                 }
-
-                if (!hasChanges)
-                    continue;
-                
-                doc.Save(path);
-                Console.WriteLine("Updated {0}!", path);
             }
+
+            Console.WriteLine("Created new resource {0}!", path);
         }
 
         private string GetFilePath(StringGroup group)
@@ -124,6 +74,93 @@ namespace Po2Resx
         private bool IsDefault(StringGroup group)
         {
             return group.Language == _defLangauge;
+        }
+
+        private void Update(ResourceInfo resource)
+        {
+            var hasChanges = false;
+            var path = resource.Path;
+            var group = resource.Group;
+
+            var doc = XDocument.Load(path);
+            var ns = XNamespace.Get(string.Empty);
+            var nsXml = XNamespace.Get("http://www.w3.org/XML/1998/namespace");
+
+            var root = doc.Root;
+            if (root == null)
+            {
+                throw new IOException(
+                    "Invalid resource file: " + path);
+            }
+
+            var existing = root
+                .Elements(ns + "data")
+                .Where(x => x.Attribute("type") == null)
+                .Select(x => new
+                {
+                    Element = x,
+                    Name = x.Attribute("name"),
+                })
+                .Where(x => x.Name != null)
+                .Select(x => new
+                {
+                    x.Element,
+                    Name = x.Name.Value,
+                })
+                .Where(x => !x.Name.StartsWith(">>"))
+                .ToDictionary(x => x.Name, x => x.Element);
+
+            if (!IsDefault(group))
+            {
+                /* Not default language
+                 * Remove untranslated keys */
+
+                var removes = existing.Keys
+                    .Except(resource.Strings.Keys)
+                    .ToList();
+
+                foreach (var remove in removes)
+                {
+                    hasChanges = true;
+                    existing[remove].Remove();
+                    existing.Remove(remove);
+                }
+            }
+
+            foreach (var item in resource.Strings)
+            {
+                XElement element;
+                if (existing.TryGetValue(item.Key, out element))
+                {
+                    var valueElement = element.Element(ns + "value");
+                    if (valueElement != null && valueElement.Value != item.Value)
+                    {
+                        hasChanges = true;
+                        valueElement.Value = item.Value;
+                    }
+                }
+                else
+                {
+                    hasChanges = true;
+                    root.Add(new XElement(ns + "data",
+                        new XAttribute("name", item.Key),
+                        new XAttribute(nsXml + "space", "preserve"),
+                        new XElement(ns + "value", item.Value)));
+                }
+            }
+
+            if (!hasChanges)
+                return;
+
+            doc.Save(path);
+            Console.WriteLine("Updated {0}!", path);
+        }
+
+        private class ResourceInfo
+        {
+            public StringGroup Group { get; set; }
+            public string Path { get; set; }
+            public Dictionary<string, string> Strings { get; set; }
         }
     }
 }
